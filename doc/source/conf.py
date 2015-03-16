c30236de33c7817238eb072b176992dd921a9de9
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-03-16 13:06:41 +0100

    [DOC] adding numpydoc extension

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
+    'numpydoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
