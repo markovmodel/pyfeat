eb025745f5677813cd01f91a1506cc0edfcf0366
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-05-05 23:12:53 +0200

    [VERSION] adding versioneer.py

--- pyfeat/__init__.py
+++ pyfeat/__init__.py
@@ -7,3 +7,7 @@ from .forge import Forge
 
 #raise API functions onto package level
 from .api import xtram, xtram_me, dtram, dtram_me, wham, wham_me, read_files, convert_data
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
