c81434cfd7dad3e4fcf5abfa66a08c291f78edc1
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-06-25 17:11:18 +0200

    [API] code cleanung and _me -> _from_matrix renaming

--- pyfeat/__init__.py
+++ pyfeat/__init__.py
@@ -6,7 +6,7 @@ from .reader import Reader
 from .forge import Forge
 
 #raise API functions onto package level
-from .api import xtram, xtram_me, dtram, dtram_me, wham, wham_me, read_files, convert_data
+from .api import xtram, xtram_from_matrix, dtram, dtram_from_matrix, wham, wham_from_matrix, read_files, convert_data
 
 from ._version import get_versions
 __version__ = get_versions()['version']
--- pyfeat/api/api.py
+++ pyfeat/api/api.py
@@ -9,7 +9,7 @@ API for the pyfeat package
 
 """
 
-from ..estimator import WHAM, XTRAM, DTRAM, TRAM #, MBAR
+from ..estimator import WHAM, XTRAM, DTRAM, TRAM
 from pytram import NotConvergedWarning, ExpressionError
 from ..reader import Reader
 from ..forge import Forge
@@ -22,18 +22,19 @@ from ..forge import Forge
 #                                                                      #
 ########################################################################
 
-def wham_me( N_K_i, b_K_i, maxiter=100, ftol=1.0E-5, verbose=False ):
+def wham_from_matrix(N_K_i, b_K_i, maxiter=100, ftol=1.0E-5, verbose=False):
     r"""
     The WHAM API function
 
     Parameters
     ----------
-    N_K_i : numpy.ndarray( shape=(T,M), dtype=numpy.float64 )
+    N_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
         total number of counts from simulation at T in M discrete Markov state (bin)
-    b_K_i : numpy.ndarray( shape=(T,M), dtype=numpy.float64 )
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
         reduced bias energies at the T thermodynamic and M discrete Markov states
     maxiter : int
-        maximum number of self-consistant iteration steps during the optimisation of the stationary probabilities
+        maximum number of self-consistant iteration steps during the optimisation
+        of the stationary probabilities
     ftol : float (> 0.0)
         convergence criterion based on the max change in an self-consistent-iteration step
     verbose : boolean
@@ -46,16 +47,16 @@ def wham_me( N_K_i, b_K_i, maxiter=100, ftol=1.0E-5, verbose=False ):
     """
     # try to create the WHAM object
     try:
-        wham_obj = WHAM( N_K_i, b_K_i )
+        wham_obj = WHAM(N_K_i, b_K_i)
     except ExpressionError, e:
         print "# ERROR ############################################################################"
         print "# Your input was faulty!"
-        print "# The < %s > object is malformed: %s" % ( e.expression, e.msg )
+        print "# The < %s > object is malformed: %s" % (e.expression, e.msg)
         print "# ABORTING #########################################################################"
         raise
     # try to converge the stationary probabilities
     try:
-        wham_obj.sc_iteration( maxiter=maxiter, ftol=ftol, verbose=verbose )
+        wham_obj.sc_iteration(maxiter=maxiter, ftol=ftol, verbose=verbose)
     except NotConvergedWarning, e:
         print "# WARNING ##########################################################################"
         print "# WHAM did not converge within %d steps!" % maxiter
@@ -66,7 +67,7 @@ def wham_me( N_K_i, b_K_i, maxiter=100, ftol=1.0E-5, verbose=False ):
         return wham_obj
 
 
-def wham( forge, maxiter=100, ftol=1.0E-5, verbose=False ):
+def wham(forge, maxiter=100, ftol=1.0E-5, verbose=False):
     r"""
     The WHAM API function
 
@@ -84,9 +85,9 @@ def wham( forge, maxiter=100, ftol=1.0E-5, verbose=False ):
     Returns
     -------
     wham_obj : object
-        wham estimator object with optimised unbiased stationary probabilities
+        WHAM estimator object with optimised unbiased stationary probabilities
     """
-    return wham_me( forge.N_K_i, forge.b_K_i, maxiter=maxiter, ftol=ftol, verbose=verbose )
+    return wham_from_matrix(forge.N_K_i, forge.b_K_i, maxiter=maxiter, ftol=ftol, verbose=verbose)
 
 ########################################################################
 #                                                                      #
@@ -94,33 +95,35 @@ def wham( forge, maxiter=100, ftol=1.0E-5, verbose=False ):
 #                                                                      #
 ########################################################################
 
-def dtram_me( C_K_ij, b_K_i, maxiter = 100, ftol = 1.0e-10, verbose = False ):
+def dtram_from_matrix(C_K_ij, b_K_i, maxiter=100, ftol=1.0e-5, verbose=False):
     r"""
     Parameters
     ----------
-    C_K_ij : numpy.ndarray( shape=(T,M,M), dtype=numpy.intc )
-        transition counts between the M discrete Markov states for each of the T thermodynamic ensembles
-    b_K_i : numpy.ndarray( shape=(T,M), dtype=numpy.float64 )
+    C_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.intc)
+        transition counts between the M discrete Markov states for each of
+        the T thermodynamic ensembles
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
         bias energies in the T thermodynamic and M discrete Markov states
+
     Returns:
     -------
     dtram_obj : obj
-        dtram estimator object with optimised stationary properties
+        dTRAM estimator object with optimised stationary properties
 
     """
 
     # try to create the DTRAM object
     try:
-        dtram_obj = DTRAM( C_K_ij, b_K_i )
+        dtram_obj = DTRAM(C_K_ij, b_K_i)
     except ExpressionError, e:
         print "# ERROR ############################################################################"
         print "# Your input was faulty!"
-        print "# The < %s > object is malformed: %s" % ( e.expression, e.msg )
+        print "# The < %s > object is malformed: %s" % (e.expression, e.msg)
         print "# ABORTING #########################################################################"
         raise
     # try to converge the stationary probabilities
     try:
-        dtram_obj.sc_iteration( maxiter=maxiter, ftol=ftol, verbose=verbose )
+        dtram_obj.sc_iteration(maxiter=maxiter, ftol=ftol, verbose=verbose)
     except NotConvergedWarning, e:
         print "# WARNING ##########################################################################"
         print "# dTRAM did not converge within %d steps!" % maxiter
@@ -136,7 +139,7 @@ def dtram_me( C_K_ij, b_K_i, maxiter = 100, ftol = 1.0e-10, verbose = False ):
 #                                                                      #
 ########################################################################
 
-def dtram( forge, lag = 1,  maxiter=100, ftol=1.0e-10, verbose= False ):
+def dtram(forge, lag=1,  maxiter=100, ftol=1.0e-5, verbose=False):
     r"""
     Parameters
     ----------
@@ -150,13 +153,14 @@ def dtram( forge, lag = 1,  maxiter=100, ftol=1.0e-10, verbose= False ):
         convergence criterion based on the max change in an self-consistent-iteration step
     verbose : boolean
         writes convergence information to stdout during the self-consistent-iteration cycle
-    
+
     Returns
     -------
     dtram_obj : object
-        dtram estimator object with optimised stationary properties
+        dTRAM estimator object with optimised stationary properties
     """
-    return dtram_me( forge.get_C_K_ij( lag), forge.b_K_i, maxiter = maxiter, ftol = ftol, verbose = verbose )
+    return dtram_from_matrix(
+        forge.get_C_K_ij(lag), forge.b_K_i, maxiter=maxiter, ftol=ftol, verbose=verbose)
     
 ########################################################################
 #                                                                      #
@@ -237,23 +241,31 @@ def dtram( forge, lag = 1,  maxiter=100, ftol=1.0e-10, verbose= False ):
 #                                                                      #
 ########################################################################
 
-def xtram_me( C_K_ij, b_K_x, T_x, M_x, N_K_i, maxiter = 100, ftol = 1.0e-10, verbose = False ):
+def xtram_from_matrix(C_K_ij, b_K_x, T_x, M_x, N_K_i, maxiter=100, ftol=1.0e-5, verbose=False):
     r"""
-    C_K_ij : numpy.ndarray( shape=(T,M,M), dtype=numpy.intc )
-        transition counts between the M discrete Markov states for each of the T thermodynamic ensembles
-    b_K_x : numpy.ndarray( shape=(T,N), dtype=numpy.float64 )
+    Parameters
+    ----------
+    C_K_ij : numpy.ndarray(shape=(T, M, M), dtype=numpy.intc)
+        transition counts between the M discrete Markov states for each of
+        the T thermodynamic ensembles
+    b_K_x : numpy.ndarray(shape=(T, N), dtype=numpy.float64)
         Biasing tensor
-    T_x : numpy.ndarray( shape=(N), dtype=numpy.intc )
+    T_x : numpy.ndarray(shape=(N,), dtype=numpy.intc)
         Thermodynamic state trajectory
-    M_x : numpy.ndarray( shape=(N), dtype=numpy.intc )
+    M_x : numpy.ndarray(shape=(N,), dtype=numpy.intc)
         Markov state trajectories
-    N_K_i : numpy.ndarray( shape=(T,M), dtype=numpy.intc )
+    N_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.intc)
         Number of markov samples in each thermodynamic state
+
+    Returns
+    -------
+    xtram_obj : object
+        xTRAM estimator object with optimised stationary properties
     """
 
     # try to create the XTRAM object
     try:
-        xtram_obj = XTRAM( C_K_ij, b_K_x, T_x, M_x, N_K_i )
+        xtram_obj = XTRAM(C_K_ij, b_K_x, T_x, M_x, N_K_i)
     except ExpressionError, e:
         print "# ERROR ############################################################################"
         print "# Your input was faulty!"
@@ -262,10 +274,10 @@ def xtram_me( C_K_ij, b_K_x, T_x, M_x, N_K_i, maxiter = 100, ftol = 1.0e-10, ver
         raise
     # try to converge the stationary probabilities
     try:
-        xtram_obj.sc_iteration( maxiter=maxiter, ftol=ftol, verbose=verbose )
+        xtram_obj.sc_iteration(maxiter=maxiter, ftol=ftol, verbose=verbose)
     except NotConvergedWarning, e:
         print "# WARNING ##########################################################################"
-        print "# WHAM did not converge within %d steps!" % maxiter
+        print "# xTRAM did not converge within %d steps!" % maxiter
         print "# The last increment was %.6e." % e.increment
         print "# You should run the < sc_iteration > method again."
         print "# USE RESULTS WITH CARE ############################################################"
@@ -279,7 +291,7 @@ def xtram_me( C_K_ij, b_K_x, T_x, M_x, N_K_i, maxiter = 100, ftol = 1.0e-10, ver
 #                                                                      #
 ########################################################################
 
-def xtram( forge, lag = 1, maxiter=100, ftol=1.0e-10, verbose= False ):
+def xtram(forge, lag=1, maxiter=100, ftol=1.0e-5, verbose=False):
     r"""
     Parameters
     ----------
@@ -297,9 +309,17 @@ def xtram( forge, lag = 1, maxiter=100, ftol=1.0e-10, verbose= False ):
     Returns
     -------
     xtram_obj : object
-        xtram estimator object with optimised stationary properties
+        xTRAM estimator object with optimised stationary properties
     """
-    return xtram_me( forge.get_C_K_ij( lag ), forge.b_K_x, forge.T_x, forge.M_x, forge.N_K_i, maxiter = maxiter, ftol = ftol, verbose = verbose )
+    return xtram_from_matrix(
+        forge.get_C_K_ij(lag),
+        forge.b_K_x,
+        forge.T_x,
+        forge.M_x,
+        forge.N_K_i,
+        maxiter=maxiter,
+        ftol=ftol,
+        verbose=verbose)
 
 
 ########################################################################
@@ -325,7 +345,7 @@ def tram():
 #                                                                      #
 ########################################################################    
     
-def convert_data( trajs, b_K_i=None, kT_K=None, kT_target=None ):
+def convert_data(trajs, b_K_i=None, kT_K=None, kT_target=None):
     r"""
     API function for creating the data forge object which will then be used for the estimators
 
@@ -333,9 +353,9 @@ def convert_data( trajs, b_K_i=None, kT_K=None, kT_target=None ):
     ----------
     trajs : list of dictionaries
         simulation trajectories
-    b_K_i : numpy.ndarray( shape=(T,M), dtype=numpy.float64 )
+    b_K_i : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
         reduced bias energies at the T thermodynamic and M discrete Markov states
-    kT_K : numpy.nparray( shape=(T), dtype=numpy.float64 )
+    kT_K : numpy.nparray(shape=(T,), dtype=numpy.float64)
         list of kT values of each thermodynamic state
     kT_target : int
         target thermodynamic state for which pi_i and f_i will be computed
@@ -345,7 +365,7 @@ def convert_data( trajs, b_K_i=None, kT_K=None, kT_target=None ):
     forge : pyfeat.Forge object
         container holding all data
     """
-    forge = Forge( trajs, b_K_i, kT_K, kT_target)
+    forge = Forge(trajs, b_K_i, kT_K, kT_target)
     return forge
 
 ########################################################################
@@ -354,7 +374,7 @@ def convert_data( trajs, b_K_i=None, kT_K=None, kT_target=None ):
 #                                                                      #
 ########################################################################
 
-def read_files (files, b_K_i_file=None, kT_file=None, skiprows=0, maxlength=None, verbose=False ):
+def read_files(files, b_K_i_file=None, kT_file=None, skiprows=0, maxlength=None, verbose=False):
     r"""
     API function for reading files 
     
@@ -376,7 +396,7 @@ def read_files (files, b_K_i_file=None, kT_file=None, skiprows=0, maxlength=None
     Returns
     -------
     reader : pyfeat.Reader object
-    	augmented trajectory information to be passed to the forge
+        augmented trajectory information to be passed to the forge
     """
     reader = Reader(files, b_K_i_file, kT_file, skiprows, maxlength, verbose)
     return reader 
